615a1a4092ba9158bc655bbfc3dd163144d4ec92
unknown <jamalnasirone52@gmail.com>
2024-08-01 16:05:44 +0000

    adding the database functionalities to the backend

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+
+import User from "@models/user";
 
 // console.log({
 //     clientId: process.env.GOOGLE_ID,
@@ -7,20 +10,45 @@ import GoogleProvider from 'next-auth/providers/google'
 // })
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  async session({ session }) {},
+
+  async signIn({ profile }) {
+    // every nextjs route is a serverless route.
+    try {
+      await connectToDB();
+
+      // check if a user already exist
+
+      const userExists = await User.findOne({
+        email: profile.email
+      })
+
+
+
+      // if not, create a user
+
+      if(!userExists) {
+        await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowercase(),
+            image: profile.image
         })
-    ],
+      }
 
-    async session({ session }){
 
-    },
-
-    async signIn({ profile }){
-        // every nextjs route is a serverless route.
+      return true;
+    } catch (error) {
+        console.log(error);
+        return false;
     }
-})
+  },
+});
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
